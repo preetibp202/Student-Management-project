1d97db9e3cb52e33343dec0961cba1285c81fbe6
Preeti Patil <161748553+preetibp202@users.noreply.github.com>
2025-01-29 18:51:26 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,37 +1,14 @@
 
-# from django.contrib import admin
-# from django.urls import path,include
-# from django.conf import settings
-# from django.conf.urls.static import static
-# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-# from shop.views import*
-
-
-
-# urlpatterns =[
-#     path('admin/', admin.site.urls),
-#     path('shop/', include('shop.urls')),  
-# ] 
-# if settings.DEBUG:  # Serve media files only in development
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from shop import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('shop/', include('shop.urls')), 
-     path('login_page/', views.login_page, name='login_page'),
-    path('register_page/', views.register_page, name='register_page'),
-     path('new_recepie', views.recepie, name='recepie'),
-    path('delete_receipe/<int:id>/', views.delete_receipe, name='delete_receipe'),
-    path('update_receipe/<int:id>/', views.update_receipe, name='update_receipe'),
-    path('logout_page/',views.logout_page,name='logout_page'),
+
     path('get_student/',views.get_student,name='get_student'),
     path('see_marks/<str:student_id>/', views.see_marks, name='see_marks'),
  
 ]
 
-if settings.DEBUG:  # Serve media files only in development
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
